55df3563d77d7cd744a359ee8000a755e82cebcc
Piotr Sitarz <piotrsitarz@onet.eu>
2019-11-02 17:38:28 +0100

    panel klienta

---
+++ assets/js/panel.js
@@ -0,0 +1,73 @@
+jQuery(function ($) {
+  $('.sidebar__option').on('click', function (e) {
+    var sidebarOption = $(e.target).closest('.sidebar__option').data('name');
+    $(".panel__content").children().hide();
+    $(`.content__${sidebarOption}`).css("display", "block");
+  });
+
+  const {
+    innerWidth
+  } = window;
+
+
+  if (innerWidth < 840) {
+    $(".sidebar__option").click(function () {
+      console.log('huehauea');
+      $(this).find(".toggle").toggle();
+    });
+  }
+
+  $(".account__icon").click(function () {
+    $(".my-account-tooltip").fadeIn();
+  });
+
+  $(document).mouseup(function (e) {
+    var myAccountTooltip = $(".my-account-tooltip");
+    if (!myAccountTooltip.is(e.target) && myAccountTooltip.has(e.target).length === 0) {
+      myAccountTooltip.fadeOut();
+    }
+  });
+
+  $(".sidebar__row").click(function () {
+    $(".sidebar__row").removeClass('active');
+    $(".sidebar__icon").removeClass('active');
+    $(".sidebar__paragraph").removeClass('active');
+    $(this).addClass('active');
+    $(this).find(".sidebar__icon").addClass('active');
+    $(this).find(".sidebar__paragraph").addClass('active');
+  });
+
+  $('.content__paragraph').on({
+    mouseenter: function () {
+      $(this).closest('.content__icon-wrapper').find('.content__hexagon').css("stroke", "#eac12d");
+      $(this).closest('.content__icon-wrapper').find('.content__icon').css("fill", "#eac12d");
+    },
+    mouseleave: function () {
+      $(this).closest('.content__icon-wrapper').find('.content__hexagon').css("stroke", "#d5d5d5");
+      $(this).closest('.content__icon-wrapper').find('.content__icon').css("fill", "#b1b1b1");
+    }
+  });
+
+  $('.content__icon').on({
+    mouseenter: function () {
+      $(this).closest('.content__icon-wrapper').find('.content__hexagon').css("stroke", "#eac12d");
+      $(this).closest('.content__icon-wrapper').find('.content__icon').css("fill", "#eac12d");
+    },
+    mouseleave: function () {
+      $(this).closest('.content__icon-wrapper').find('.content__hexagon').css("stroke", "#d5d5d5");
+      $(this).closest('.content__icon-wrapper').find('.content__icon').css("fill", "#b1b1b1");
+    }
+  });
+
+  $('.content__hexagon').on({
+    mouseenter: function () {
+      $(this).closest('.content__icon-wrapper').find('.content__hexagon').css("stroke", "#eac12d");
+      $(this).closest('.content__icon-wrapper').find('.content__icon').css("fill", "#eac12d");
+    },
+    mouseleave: function () {
+      $(this).closest('.content__icon-wrapper').find('.content__hexagon').css("stroke", "#d5d5d5");
+      $(this).closest('.content__icon-wrapper').find('.content__icon').css("fill", "#b1b1b1");
+    }
+  });
+
+});
